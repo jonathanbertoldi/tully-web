f9f12b2d7d77ffe12b99c31417d1ede719f78034
Jonathan Bertoldi <jonathanbertoldi7@gmail.com>
2017-08-24 17:18:10 -0300

    Variáveis de ambiente

--- src/api/apiUtils.js
+++ src/api/apiUtils.js
@@ -1,7 +1,7 @@
 const developmentUrl = 'http://localhost:8080/api/';
 const productionUrl = 'https://tully-api.herokuapp.com/api/';
 
-export const apiUrl = process.env.NODE_ENV === 'Production' ? productionUrl : developmentUrl;
+export const apiUrl = process.env.NODE_ENV === 'development' ? developmentUrl : productionUrl;
 
 export function apiError(statusCode, message, url) {
   return {
